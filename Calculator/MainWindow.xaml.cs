8b2c04d4ced91ec8e671f02e016d22a84d132072
Cryafonic <Brendonvandernest69@gmail.com>
2020-03-16 21:52:42 +0200

    The start of the calculator.

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -18,16 +18,30 @@ namespace Calculator
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class App : Window
+    public partial class MainWindow : Window
     {
         private Operator currentOperator;
         private double firstNumber;
         private double secondNumber;
 
 
-        public App()
+        public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox Text = new TextBox();
+            var btn = sender as Button;
+            if (txtResult.Text != "0")
+            {
+                txtResult.Text = $"{txtResult.Text}{btn.Content}";
+            }
+            else
+            {
+                txtResult.Text = btn.Content.ToString();
+            }
         }
 
         private void Button_Click_AC(Object sender, RoutedEventArgs e)
@@ -35,8 +49,70 @@ namespace Calculator
             firstNumber = 0;
             secondNumber = 0;
             currentOperator = 0;
+            txtResult = "0";
+        }
+
+        private void Button_Click_Plus(object sender, RoutedEventArgs e)
+        {
+            currentOperator = Operator.Add;
+            firstNumber = double.Parse(txtResult.Text);
             txtResult.Text = "0";
         }
+
+        private void Button_Click_Percentage(object sender, RoutedEventArgs e)
+        {
+            secondNumber = double.Parse(txtResult.Text);
+            txtResult.Text = GetResult(firstNumber, currentOperator, secondNumber);
+        }
+
+        private void Button_Click_Minus(object sender , RoutedEventArgs e)
+        {
+            currentOperator = Operator.Substract;
+            firstNumber = double.Parse(txtResult.Text);
+            txtResult.Text = "0";
+        }
+
+        private void Button_Click_Multiply(object sender, RoutedEventArgs e)
+        {
+            currentOperator = Operator.Multiply;
+            firstNumber = double.Parse(txtResult.Text);
+            txtResult.Text = "0";
+        }
+
+        private void Button_Click_divide(object sender, RoutedEventArgs e)
+        {
+            currentOperator = Operator.Divide;
+            firstNumber = double.Parse(txtResult.Text);
+            txtResult.Text = "0";
+        }
+
+
+        private string GetResult(double firstNumber, Operator currentOperator, double secondNumber) 
+        {
+            if (currentOperator == Operator.Add)
+            {
+                return (firstNumber + secondNumber).ToString();
+            }
+            else if (currentOperator == Operator.Substract)
+            {
+                return (firstNumber - secondNumber).ToString();
+            }
+            else if (currentOperator == Operator.Multiply)
+            {
+                return (firstNumber * secondNumber).ToString();
+            }
+            else if (currentOperator == Operator.Divide)
+            {
+                return (firstNumber / secondNumber).ToString();
+            }
+            else
+            {
+                return "0";
+            }
+        }
+
+
+
         public enum Operator
         { 
             Add,
